1c7a893ea446b9e60a1bd7ecc4b747dd098be308
Khanh Nguyen <khanh.nguyen@frosmo.com>
2020-01-29 00:51:07 +0200

    Fix window undefined

--- src/shared/WindowDimensionsProvider.js
+++ src/shared/WindowDimensionsProvider.js
@@ -3,8 +3,8 @@ import React, { createContext, useContext, useState, useEffect } from 'react';
 const WindowDimensionsContext = createContext(null);
 const WindowDimensionsProvider = ({ children }) => {
     const [dimensions, setDimensions] = useState({
-        width: window.innerWidth,
-        height: window.innerHeight,
+        width: 0,
+        height: 0,
     });
     useEffect(() => {
         const handleResize = () => {
