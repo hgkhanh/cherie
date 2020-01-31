0d2b577d8793a036a33b583512cd289916a9e30e
Khanh Nguyen <khanh.nguyen@frosmo.com>
2020-01-31 16:44:33 +0200

    Don not build page for slide md

--- gatsby-node.js
+++ gatsby-node.js
@@ -38,7 +38,7 @@ exports.createPages = async ({ graphql, actions }) => {
   // Get a full list of markdown products
   const markdownQueryResult = await graphql(`
     {
-      allMarkdownRemark {
+      allMarkdownRemark(filter: {fileAbsolutePath: {regex: "/products\\//"}}) {
         edges {
           node {
             id
