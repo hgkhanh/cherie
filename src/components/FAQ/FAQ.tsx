05efccde635ec31181c79deb61e47f2d31569e4c
Khanh Nguyen <khanh.nguyen@frosmo.com>
2020-02-25 00:01:09 +0200

    FAQ init

---
+++ src/components/FAQ/FAQ.tsx
@@ -0,0 +1,32 @@
+import React from "react";
+import * as styles from "./FAQ.module.scss";
+
+const FAQ = () => {
+  return (
+    <div className={styles.container}>
+      <div className={styles.qaBlock}>
+        <h2>How far in advance should I begin searching for dresses?</h2>
+        <p>In order to ensure you are not rushed with your decision-making,
+        we are recommend you to begin</p>
+      </div>
+
+      <div className={styles.qaBlock}>
+        <h2>How far in advance should I begin searching for dresses?</h2>
+        <p>In order to ensure you are not rushed with your decision-making,
+        we are recommend you to begin</p>
+      </div>
+      <div className={styles.qaBlock}>
+        <h2>How far in advance should I begin searching for dresses?</h2>
+        <p>In order to ensure you are not rushed with your decision-making,
+        we are recommend you to begin</p>
+      </div>
+      <div className={styles.qaBlock}>
+        <h2>How far in advance should I begin searching for dresses?</h2>
+        <p>In order to ensure you are not rushed with your decision-making,
+        we are recommend you to begin</p>
+      </div>
+    </div>
+  );
+}
+
+export default FAQ;
---
+++ src/pages/faq.tsx
@@ -0,0 +1,23 @@
+import React, { Component } from "react";
+import Helmet from "react-helmet";
+import Layout from "../layout";
+import FAQ from "../components/FAQ";
+import config from "../../data/SiteConfig";
+
+const AboutPage = () => {
+  return (
+    <Layout>
+      <Helmet title={`FAQ | ${config.siteTitle}`} />
+      <hr className='divider' />
+      <div className='grid'>
+        <h1 className="centerAlign">
+          FAQ
+        </h1>
+        <FAQ />
+      </div>
+      <hr className='divider' />
+    </Layout>
+  );
+}
+
+export default AboutPage;
