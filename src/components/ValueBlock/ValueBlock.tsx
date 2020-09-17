09781bf04bd7e24ce7a89d2ba6345bf850ab66e1
Khanh Nguyen <khanh.nguyen@frosmo.com>
2020-09-17 17:39:13 +0300

    Value block done

--- src/components/ValueBlock/ValueBlock.tsx
+++ src/components/ValueBlock/ValueBlock.tsx
@@ -2,11 +2,9 @@ import React, { useContext } from 'react';
 import { useStaticQuery, graphql } from 'gatsby';
 import Hero from '../Hero';
 import styles from './ValueBlock.module.scss';
-import { WindowDimensionsContext } from "../../shared/WindowDimensionsProvider";
-
+import cx from 'classnames';
 
 const ValueBlock = () => {
-    const { width } = useContext(WindowDimensionsContext);
     const data = useStaticQuery(graphql`
         query {
             background1: file(name: {eq: "Value-01"}) {
@@ -27,84 +25,43 @@ const ValueBlock = () => {
         }
     `);
 
-    if (width < 900) {
-        return (
-            <div className='gridWrapper'>
-                <div className='grid wide'>
-                    <div className='sectionTitle centerAlign' style={{ margin: '10px 0' }}>
-                        <span>The better and better</span>
-                    </div>
-                    <div style={{ height: 300 }}>
+    return (
+        <div>
+            <div className='sectionTitle centerAlign' style={{ margin: '10px 0' }}>
+                <h3 style={{ marginBottom: '0' }}>The better and better</h3>
+            </div>
+            <div className={cx(styles.cardContainer, styles.right)} style={{ paddingTop: '0' }}>
+                <div className={styles.card}>
+                    <div className={styles.image}>
                         <Hero overlay={false} hasScroll={false} isParallax={false}
                             imageWide={data.background1} mobileFullHeight={false}>
                         </Hero>
                     </div>
-                    <div className='gridWrapper' style={{ marginTop: '20px' }}>
-                        <div className='grid narrow'>
-                            <div className='sectionTextBlock centerAlign'>
-                                <h3 className='uppercase heavyText'>Better Experience</h3>
-                                <p>
-                                    A less painful, less stressful way to find 'the' dress.
-                    </p>
-                            </div>
-                        </div>
+                    <div className={styles.text}>
+                        <h3 className='uppercase heavyText'>Better Experience</h3>
+                        <p>
+                            A less painful, less stressful way to find 'the' dress.
+                            </p>
                     </div>
-                    <hr className='divider' />
-                    <div style={{ height: 300 }}>
-                        <Hero overlay={false} hasScroll={false} isParallax={false}
-                            imageWide={data.background2} mobileFullHeight={false}>
-                        </Hero>
-                    </div>
-                    <div className='gridWrapper' style={{ marginTop: '20px' }}>
-                        <div className='grid narrow'>
-                            <div className='sectionTextBlock centerAlign'>
-                                <h3 className='uppercase heavyText'>Better Price</h3>
-                                <p>
-                                    High quality with unmatched price.
-                    </p>
-                            </div>
-                        </div>
-                    </div>
-                    <hr className='divider' />
                 </div>
             </div>
-        )
-    } else {
-        return (
-            <React.Fragment>
-                <div className='sectionTitle centerAlign' style={{ margin: '10px 0' }}>
-                    <h2>The better and better</h2>
-                </div>
-                <div className={styles.container}>
-                    <div className={styles.image} style={{ height: 500 }}>
-                        <Hero overlay={false} hasScroll={false} isParallax={false}
-                            imageWide={data.background1} mobileFullHeight={false}>
-                        </Hero>
-                    </div>
-                    <div className={styles.text}>
-                        <h1 className='uppercase heavyText'>Better Experience</h1>
-                        <h2 className='grayText'>
-                            A less painful, less stressful way to find 'the' dress.
-                        </h2>
-                    </div>
-                </div>
-                <div className={styles.container} >
-                    <div className={styles.text}>
-                        <h1 className='uppercase heavyText'>Better Price</h1>
-                        <h2 className='grayText'>
-                            High quality with unmatched price.
-                        </h2>
-                    </div>
-                    <div className={styles.item} style={{ height: 500 }}>
+            <div className={cx(styles.cardContainer, styles.left)}>
+                <div className={styles.card}>
+                    <div className={styles.image}>
                         <Hero overlay={false} hasScroll={false} isParallax={false}
                             imageWide={data.background2} mobileFullHeight={false}>
                         </Hero>
                     </div>
-
+                    <div className={styles.text}>
+                        <h3 className='uppercase heavyText'>Better Price</h3>
+                        <p>
+                            High quality with unmatched price.
+                            </p>
+                    </div>
                 </div>
-            </React.Fragment >
-        )
-    }
+            </div>
+        </div>
+    )
 };
 
 
